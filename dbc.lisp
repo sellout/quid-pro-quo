4b879eb8d6c3ee94502a33e9b8b74f55d0853103
Greg Pfeil <greg@technomadic.org>
2011-04-14 14:07:31 -0400

    Refactored invariant method creation outside of initialize-instance.

--- dbc.lisp
+++ dbc.lisp
@@ -173,45 +173,43 @@
                :object object
                :description (documentation invariant 'function))))
 
-(defun passes-effective-invariants-p (object)
+(defun passes-class-invariants-p (object)
   (loop for invariant in (effective-class-invariants (class-of object))
      if (not (funcall invariant object))
      return nil
      finally (return t)))
 
+(defun add-reader-invariant (reader class)
+  (add-method (ensure-generic-function reader
+                                       :lambda-list '(object)
+                                       :method-combination '(contract))
+              (make-instance 'standard-method
+                             :qualifiers '(:invariant)
+                             :lambda-list '(object)
+                             :specializers (list class)
+                             :function #'passes-class-invariants-p)))
+
+(defun add-writer-invariant (writer class)
+  (add-method (ensure-generic-function writer
+                                       :lambda-list '(new-value object)
+                                       :method-combination '(contract))
+              (make-instance 'standard-method
+                             :qualifiers '(:invariant)
+                             :lambda-list '(new-value object)
+                             :specializers (list (find-class t) class)
+                             :function (lambda (new-value object)
+                                         (declare (ignore new-value))
+                                         (passes-class-invariants-p object)))))
+
 (defmethod initialize-instance :after
     ((instance contracted-class) &key invariants &allow-other-keys)
   (setf (slot-value instance 'invariants) (mapcar #'eval invariants))
   ;; FIXME: need to do this for all slots, not just direct slots
-  (let* ((slots (class-direct-slots instance))
-         (readers (reduce #'append (mapcar #'slot-definition-readers slots)))
-         (writers (reduce #'append (mapcar #'slot-definition-writers slots))))
-    (mapc (lambda (reader)
-            (add-method (ensure-generic-function
-                         reader
-                         :lambda-list '(object)
-                         :method-combination '(contract))
-                        (make-instance
-                         'standard-method
-                         :qualifiers '(:invariant)
-                         :lambda-list '(object)
-                         :specializers (list instance)
-                         :function (lambda (object)
-                                     (passes-effective-invariants-p object)))))
-          readers)
-    (mapc (lambda (writer)
-            (add-method (ensure-generic-function writer
-                         :lambda-list '(new-value object)
-                         :method-combination '(contract))
-                        (make-instance
-                         'standard-method
-                         :qualifiers '(:invariant)
-                         :lambda-list '(new-value object)
-                         :specializers (list (find-class t) instance)
-                         :function (lambda (new-value object)
-                                     (declare (ignore new-value))
-                                     (passes-effective-invariants-p object)))))
-          writers)))
+  (let ((slots (class-direct-slots instance)))
+    (mapc (lambda (reader) (add-reader-invariant reader instance))
+          (reduce #'append (mapcar #'slot-definition-readers slots)))
+    (mapc (lambda (writer) (add-writer-invariant writer instance))
+          (reduce #'append (mapcar #'slot-definition-writers slots)))))
 
 (defmethod reinitialize-instance :after
     ((instance contracted-class) &key invariants &allow-other-keys)
