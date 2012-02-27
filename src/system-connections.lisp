e057fa644d4bc511c2ceec6c1dc5804ed0f18bb8
Greg Pfeil <greg@technomadic.org>
2012-02-27 08:43:34 -0700

    Make sure we look in the right place for LOOM support.

--- src/system-connections.lisp
+++ src/system-connections.lisp
@@ -2,4 +2,6 @@
 
 (defsystem-connection quid-pro-quo.slot-value-invariants
   :requires (quid-pro-quo loom)
+  :pathname #.(merge-pathnames "src/"
+                               (asdf:system-source-directory :quid-pro-quo))
   :components ((:file "loom-slot-value-invariants")))
