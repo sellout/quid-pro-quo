eeb7701a6ede3a79de5112718584a1d9f4f03d96
Greg Pfeil <greg@technomadic.org>
2012-03-21 20:46:04 -0600

    This was part of the last commit, but didn't notice until after the push.

---
+++ src/macros.lisp
@@ -0,0 +1,41 @@
+(in-package #:quid-pro-quo)
+
+(defmacro defrequirement (name (&rest lambda-list) &body body)
+  "Adds a precondition to the NAMEd function. It can be either a generic or
+   non-generic function. If it's the former, then use a specialized lambda list,
+   otherwise use an ordinary lambda list. The docstring, if any, will be used in
+   failure reports."
+  (multiple-value-bind (remaining-forms declarations doc-string)
+      (parse-body body :documentation t)
+    (declare (ignore remaining-forms declarations))
+    (let ((method `(defmethod ,name
+                       :require ,@(when doc-string (list doc-string))
+                       ,lambda-list
+                       ,@body)))
+      (if (every #'symbolp lambda-list)
+          `(if (and (fboundp ',name)
+                    (not (typep (fdefinition ',name) 'generic-function)))
+               (defcontract ,name :require ,lambda-list
+                 ,@body)
+               ,method)
+          method))))
+
+(defmacro defguarantee (name (&rest lambda-list) &body body)
+  "Adds a postcondition to the NAMEd function. It can be either a generic or
+   non-generic function. If it's the former, then use a specialized lambda list,
+   otherwise use an ordinary lambda list. The docstring, if any, will be used in
+   failure reports."
+  (multiple-value-bind (remaining-forms declarations doc-string)
+      (parse-body body :documentation t)
+    (declare (ignore remaining-forms declarations))
+    (let ((method `(defmethod ,name
+                       :guarantee ,@(when doc-string (list doc-string))
+                       ,lambda-list
+                       ,@body)))
+      (if (every #'symbolp lambda-list)
+          `(if (and (fboundp ',name)
+                    (not (typep (fdefinition ',name) 'generic-function)))
+               (defcontract ,name :guarantee ,lambda-list
+                 ,@body)
+               ,method)
+          method))))
